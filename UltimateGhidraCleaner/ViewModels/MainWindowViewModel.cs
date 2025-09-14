f23a52ffd273aa3064badaad6960024f9e1b943a
Dracau <supremfire0@gmail.com>
2025-09-14 22:50:07 +0200

    Namespaces, Casts, Hashes, Fighter stuff, gotos

--- UltimateGhidraCleaner/ViewModels/MainWindowViewModel.cs
+++ UltimateGhidraCleaner/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -13,8 +15,11 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private bool _shouldRemoveImpl = true;
     [ObservableProperty] private bool _shouldReplaceUnderscores = true;
     [ObservableProperty] private bool _shouldCleanupConditions = true;
-
-    //[ObservableProperty] private bool _shouldCommentLABFUNGotos = true;
+    [ObservableProperty] private bool _shouldCleanupNamespaces = true;
+    [ObservableProperty] private bool _shouldRemoveCasts = true;
+    [ObservableProperty] private bool _shouldReplaceThis = true;
+    [ObservableProperty] private bool _shouldCommentLABGotos = true;
+    [ObservableProperty] private bool _shouldReplaceHashes = true;
 
     [RelayCommand]
     public void Clean()
@@ -28,6 +33,11 @@ public partial class MainWindowViewModel : ViewModelBase
         if (ShouldRemoveImpl) buffer = RemoveImpl(buffer);
         if (ShouldReplaceUnderscores) buffer = ReplaceUnderscores(buffer);
         if (ShouldCleanupConditions) buffer = CleanupConditions(buffer);
+        if (ShouldCleanupNamespaces) buffer = CleanupNamespaces(buffer);
+        if (ShouldRemoveCasts) buffer = RemoveCasts(buffer);
+        if (ShouldReplaceThis) buffer = ThisBecomesFighter(buffer);
+        if (ShouldCommentLABGotos) buffer = CommentLABgotos(buffer);
+        if (ShouldReplaceHashes) buffer = ReplaceHashes(buffer);
         
         Output = buffer;
     }
@@ -41,7 +51,7 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             if (input[j] == '\n')
             {
-                start = j-1;
+                start = j;
                 break;
             }
         }
@@ -88,6 +98,72 @@ public partial class MainWindowViewModel : ViewModelBase
 
     private string CleanupConditions(string input)
     {
-        return input.Replace(" & 1U", "").Replace(" & 1", "");
+        return input.Replace(" & 1U", "").Replace(" & 1", "").Replace(" != 0","");
+    }
+    
+    private string CleanupNamespaces(string input)
+    {
+        return input.Replace("lib::L2CValue::", "")
+            .Replace("app::lua_bind::", "")
+            .Replace("app::sv_battle_object::", "")
+            .Replace("lib::L2CAgent::", "")
+            .Replace("lua2cpp::L2CFighterBase::", "") //Maybe a separate function for fighter stuff
+            .Replace("lua2cpp::L2CFighterCommon::", "");
+    }
+
+    private string RemoveCasts(string input)
+    {
+        return input.Replace("operator.cast.to.bool", "")
+            .Replace("as_integer","")
+            .Replace("(bool)", "")
+            .Replace("(float)", "")
+            .Replace("(BattleObjectModuleAccessor **)", "")
+            .Replace("(L2CValue *)","")
+            .Replace("(L2CValue)","")
+            .Replace("((L2CAgent *))","");
+    }
+
+    private string ThisBecomesFighter(string input)
+    {
+        return input.Replace("this->", "fighter.").Replace("this", "fighter");
+    }
+
+    private string CommentLABgotos(string input)
+    {
+        return input.Replace("\nLAB","\n//LAB").Replace("goto LAB", "//goto LAB");
+    }
+
+    private static string labelsPath = "C:\\Users\\supre\\Desktop\\Smash Mods\\Outils\\Ghidra Reqs\\ParamLabels.csv";
+    private static string allowedCharsInHashes = "0123456789ABCDEFabcdef";
+    
+    Dictionary<string, string> hashDictionary = new Dictionary<string, string>();
+    private string ReplaceHashes(string input)
+    {
+        if (hashDictionary.Count == 0)
+        {
+            string[] lines = File.ReadAllLines(labelsPath);
+            foreach (string line in lines)
+            {
+                string[] lineSplits = line.Split(',');
+                hashDictionary.Add(lineSplits[0],lineSplits[1]);
+            }
+        }
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            i = input.IndexOf("0x",i);
+            if(i == -1) break;
+            int hashLength = 2;
+            for (int j = i + 2; j < input.Length; j++)
+            {
+                if(!allowedCharsInHashes.Contains(input[j])) break;
+                hashLength++;
+            }
+            string hash = input.Substring(i, hashLength);
+            
+            if(hashDictionary.ContainsKey(hash)) input = input.Replace(hash,$"hash40(\"{hashDictionary[hash]}\")");
+        }
+
+        return input;
     }
 }
--- UltimateGhidraCleaner/obj/Debug/net9.0/UltimateGhidraCleaner.AssemblyInfo.cs
+++ UltimateGhidraCleaner/obj/Debug/net9.0/UltimateGhidraCleaner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UltimateGhidraCleaner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5dd413814eceb494f0c38ff28bb58d645deba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56c3ba737848791899788a08ea244c47a4ac3557")]
 [assembly: System.Reflection.AssemblyProductAttribute("UltimateGhidraCleaner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UltimateGhidraCleaner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
